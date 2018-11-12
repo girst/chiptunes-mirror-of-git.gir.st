ab6fe4c2d3c57df6db00cb72b510f49b174d5ea7
Tobias Girstmair <t@thi3nkpad.lan>
2018-11-13 00:00:46 +0100

    new version

--- foo.c
+++ foo.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 typedef unsigned char u8;
 int g(int i, int x, int t, int o) {
-	return ((3 & x & (i * ((3 & i >> 16 ? "BY}6YB6%" : "Qj}6jQ6%")[t % 8] + 51) >> o)) << 4);
+	return ((3 & x & (i * ((3 & i >> 16 ? "BY}6YB6%" : "Qj}6jQ6%")[t % 8] + 51) >> o)));
 };
 
 int main(void) {
 	int i;
 	int n;
 	int s;
+	u8 acc;
 	for (i = 0;; i++) {
+		acc = 0;
 		n = i >> 14;
 		s = i >> 17;
-		putchar(g(i, 1, n, 12) +
-			g(i, s, n ^ i >> 13, 10) + 
-			g(i, s / 3, n + ((i >> 11) % 3), 10) + 
-			g(i, s / 5, 8 + n - ((i >> 10) % 3), 9));
+		acc += g(i, 1, n, 12);
+		acc += g(i, s, n ^ i >> 13, 10);
+		acc += g(i, s / 3, n + ((i >> 11) % 3), 10);
+		acc += g(i, s / 5, 8 + n - ((i >> 10) % 3), 9);
+		putchar(acc<<4);
 	}
 }
